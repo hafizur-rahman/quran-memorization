0521b0a1b7f64837bd3162e6e7faa4eb92246ef5
Hafizur Rahman <bibagimon@gmail.com>
2025-08-29 11:56:25 +0900

    Fix for JavaFX 21

--- src/main/java/com/dreamer/util/PageModelImpl.java
+++ src/main/java/com/dreamer/util/PageModelImpl.java
@@ -2,6 +2,7 @@ package com.dreamer.util;
 
 import javafx.scene.image.Image;
 
+import java.io.FileInputStream;
 import java.nio.file.Path;
 
 public class PageModelImpl implements PageModel {
@@ -17,11 +18,13 @@ public class PageModelImpl implements PageModel {
 
     public Image getImage(int pageNumber) {
         Image pageImage;
+
+        Path file = Path.of(imageDirPath.toString(), String.format("/p%03d.gif", pageNumber));
+
         try {
-            String file = Path.of(imageDirPath.toString(), String.format("/p%03d.gif", pageNumber)).toString();
-            pageImage = new Image(file);
+            pageImage = new Image(new FileInputStream(file.toFile()));
         } catch (Exception ex) {
-            throw new RuntimeException("Page image not found: " + pageNumber);
+            throw new RuntimeException("Page image not found: " + file);
         }
         return pageImage;
     }
